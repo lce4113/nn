d2a853ff8abbc457d6cc2f7348fbeddb202f0199
Om Mahesh <2om.mahesh@gmail.com>
2021-07-17 18:13:52 -0700

    Did a create-react-app

---
+++ src/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.scss';
+import App from './App';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <noscript>You need to enable JavaScript to run this app.</noscript>
+    <App />
+  </React.StrictMode>,
+  document.getElementsByTagName('body')[0]
+);
